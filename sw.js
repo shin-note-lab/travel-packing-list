f1205c7f0ff6fbb1bd83d4ed9389c78612d9bb70
shin-note-lab <n.hikaru17@gmail.com>
2026-09-07 15:07:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="travel-packing-v10";
+const CACHE_NAME="travel-packing-v10-1";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 
 self.addEventListener("install",event=>{
